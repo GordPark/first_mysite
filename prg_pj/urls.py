d90fd10adfbc4c5257805adce9fb733d37f320fe
GordPark <gord23p@gmail.com>
2024-04-02 15:51:14 +0900

    generic View로 views.py 업데이트

--- prg_pj/urls.py
+++ prg_pj/urls.py
@@ -7,14 +7,28 @@ from . import views
 app_name = "prg_pj"
 urlpatterns = [
     # 루트 URL "" , views.index에서 호출하고 패턴의 이름은 index
-    path("", views.index, name="index"),
+    # path("", views.index, name="index"),
+    
+    path("", views.IndexView.as_view(), name="index"),
     # path("", views.index1, name="index1"),
     #int형 question_id를 받는 URL에 대한 요청을 처리, views.detail에서 호출하고 패턴 이름은 detail
     # /polls/5/  와 같은형식이고 세부 정보를 세부 정보를 보여줌
-    path("<int:question_id>/", views.detail, name="detail"),
+    # path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:q_id>/", views.DetailView.as_view(), name="detail"),
     # int형 question_id를 받는 URL에 대한 요청을 처리, views.results를 호출하고 
     # /polls/5/results/ 와 같은 형식
-    path("<int:question_id>/results/", views.results, name="results"),
+    # path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:pk>/recently/", views.RecentlyView.as_view(), name="recently"),
+    path("<int:pk>/mostvoted/", views.MostVotedView.as_view(), name="mostvoted"),
+    path("<int:pk>/unvoted/", views.UnVotedView.as_view(), name="unvoted"),
+    path("question/new/", views.QuestionCreateView.as_view(), name="question_new"),
+    path("question/<int:pk>/choice/new/", views.ChoiceCreateView.as_view(), name="choice_new"),
+    path("question/<int:pk>/update/", views.QuestionUpdateView.as_view(), name="question_update"),
+    path("choice/<int:pk>/update/", views.ChoiceUpdateView.as_view(), name="choice_update"),
+    path('question/<int:pk>/delete/', views.QuestionDeleteView.as_view(), name='question_delete'),
+    
+
     # int형 question_id를 받는 URL에 대한 요청을 처리, views.results를 호출하고 
     # /polls/5/vote/ 와 같은 형식
     path("<int:question_id>/vote/", views.vote, name="vote"),
@@ -24,5 +38,6 @@ urlpatterns = [
 
     path("ex/", views.ex_index, name="ex_index"),
     path("ex/<int:question_id>/", views.ex_detail, name="ex_detail"),
+
 
 ]
--- prg_pj/views.py
+++ prg_pj/views.py
@@ -4,10 +4,15 @@ from .models import Question, Choice
 from django.template import loader
 from django.http import Http404, HttpResponseRedirect
 from datetime import datetime
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils import timezone
+from django.views import generic
+from django.db.models import Sum, Max
+# 원하는 클래스 가져오기
+from django.views.generic import UpdateView
 
 # Create your views here.
+
 # def index(request):   
 #     latest_question_list = Question.objects.order_by("-pub_date")[:5]
 #     template = loader.get_template("prg_pj/index.html")
@@ -21,10 +26,121 @@ from django.utils import timezone
 #     latest_question_list = Question.objects.order_by("-pub_date")[:5]
 #     context = {"latest_question_list": latest_question_list}
 #     return render(request, "prg_pj/index.html", context)
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:10]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "prg_pj/index.html", context)
+
+# Choice - UpdateView
+class ChoiceUpdateView(generic.edit.UpdateView):
+    model = Choice
+    fields = ['choice_text'] # , 'pub_date'
+    template_name = 'prg_pj/choice_update_form.html'   # 새로운 템플릿 또는 기존 템플릿 지정
+
+    def get_success_url(self):
+        # 선택지가 업데이트된 후, 선택지가 속한 질문의 상세 페이지로 리다이렉션
+        choice = self.object
+        ## 리다이렉트 문제
+        return reverse('prg_pj:detail', kwargs={'question_id': choice.question.pk})
+
+# Question - UpdateView
+class QuestionUpdateView(generic.edit.UpdateView):
+    model = Question
+    fields = ['question_text'] # , 'pub_date'
+    template_name = 'prg_pj/question_update_form.html'  # 재사용하거나 적절한 템플릿 지정
+    success_url = reverse_lazy('prg_pj:index')  # 예시 URL, 실제 프로젝트에 맞게 수정 필요
+
+
+# Question - DeleteView
+class QuestionDeleteView(generic.edit.DeleteView):
+    model = Question
+    template_name = 'prg_pj/question_confirm_delete.html'
+    success_url = reverse_lazy('prg_pj:index')  # 삭제 후 리다이렉션될 URL, 실제 프로젝트에 맞게 수정 필요
+
+# ChoiceView
+class ChoiceCreateView(generic.edit.CreateView):
+    model = Choice
+    fields = ['choice_text']    
+    template_name = 'prg_pj/choice_form.html'
+    def form_valid(self,form):
+        form.instance.question = get_object_or_404(Question, pk=self.kwargs['pk'])
+        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse('prg_pj:detail', kwargs={'question_id': self.kwargs['pk']})
+    # 정적인 주소
+    # success_url = reverse_lazy('prg_pj:index')
+
+# CreateView
+class QuestionCreateView(generic.edit.CreateView):
+    model = Question
+    fields = ['question_text'] # , 'pub_date'
+    template_name = 'prg_pj/question_form.html'
+    success_url = reverse_lazy('prg_pj:index')  # 예시 URL, 실제 프로젝트에 맞게 수정 필요
+
+
+# IndexView
+class IndexView(generic.ListView):
+    # [app_name]/[model_name]_list.html
+    # templates폴더안에 html 이름을 _list로 끝나게 만들면 template_name을 지정안해도 됨
+    template_name = "prg_pj/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last fiv publisehd questions."""
+        return Question.objects.order_by("-pub_date")[:10]
+
+# DetailView
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "prg_pj/detail.html"
+    def get_object(self):                
+        q_id = self.kwargs['q_id']
+        question = get_object_or_404(Question, pk=q_id)
+        return question
+
+
+
+
+# ResultsView
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "prg_pj/results.html"
+
+# 최근 설문조사 질문 목록
+class RecentlyView(generic.ListView):
+    model = Question
+    template_name = "prg_pj/recently.html"
+    context_object_name = "latest_question_list"
+    # queryset = Question.objects.all()  # 새로운 queryset를 정의함
+
+
+    def get_queryset(self):
+        """Return the last fiv publisehd questions."""
+        return Question.objects.order_by("-pub_date")[:3]
+    
+# 가장 많은 투표를 받은 질문
+# 전체 퀘스쳔 가져오기
+# 각 퀘스쳔 별로 votes를 어떻게 구하지
+# 총합을 구하기
+# 정렬은 어떻게 할까
+# 하나 question -> q.choice_set.all() : choice들 -> sum[] 리스트컴프리헨션
+
+class MostVotedView(generic.DetailView):
+    model = Choice
+    template_name = "prg_pj/mostvoted.html"    
+    most_voted = Choice.objects.aggregate(most_voted=Max('votes'))
+
+
+    def get_queryset(self):
+        """Return the last fiv publisehd questions."""
+        return Choice.objects.order_by('votes')[:5]
+
+
+# 아직 투표가 없는 질문 목록
+class UnVotedView(generic.DetailView):
+    model = Choice
+    template_name = "prg_pj/unvoted.html"
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:10]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "prg_pj/index.html", context)
 
 # index1을 만들고, index1.html을 만들어서 동작시켜 보기
 def ex_index(request):
@@ -34,9 +150,9 @@ def ex_index(request):
         }
     return render(request, "prg_pj/ex.html", context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "prg_pj/detail.html", {"question": question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "prg_pj/detail.html", {"question": question})
 
 def ex_detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -68,8 +184,11 @@ def return_Hello(request):
     response = "Hello World!"
     return HttpResponse(response)
 
-def results(request, question_id):    
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+# def results(request, question_id):    
+#     question = get_object_or_404(Question, pk=question_id)
+    
+#     return render(request, "prg_pj/results.html",  {"question":question})
+    # return HttpResponse("You're looking at the results of question %s." % question_id)
 
 from django.db.models import F
 def vote(request, question_id):
@@ -89,6 +208,15 @@ def vote(request, question_id):
         )
     else:
         selected_choice.votes = F("votes") + 1
+        # 모든 선택지의 투표수 1증가
+        Choice.objects.filter(question_id=question_id).update(votes=F('votes')+1)
+        # 특정 선택지의 투표 수 감소
+        Choice.objects.filter(question_id=question_id).update(votes=F('votes')-1)
+        
+        
+        # 모든 선택지의 튜표 두배로 늘리기
+        Choice.objects.filter(question_id=question_id).update(votes=F('votes')*2)
+
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
